6946745a401434afa87a08b576dc264b38befc3a
Azemina <Sil220044@spengergasse.at>
2026-04-09 15:09:22 +0200

    Aufgabe10

---
+++ src/main/java/at/spengergasse/spring_thymeleaf/entities/Device.java
@@ -0,0 +1,53 @@
+package at.spengergasse.spring_thymeleaf.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Device {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;      // Bezeichnung
+    private String type;      // Art des Geräts (MR, CT, Röntgen)
+    private String location;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+
+}
